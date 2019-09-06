a5f42bc3630972cafa678792f9b791afae587ebd
Thiyagarajah, Mayurathan <Mayurathan.Thiyagarajah@nav.no>
2019-09-06 15:20:46 +0200

    TFP-1051: Baseline for fplos

--- migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.0_56__PFP_6238_AktivFeltOppgaveEgenskap.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE OPPGAVE_EGENSKAP ADD AKTIV VARCHAR2(1 CHAR) DEFAULT 'J' NOT NULL;
-COMMENT ON COLUMN OPPGAVE.AKTIV IS 'Setter oppgaveegenskap til aktiv eller inaktiv etter egenskapen sin tilstand';
--- migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.0_62__PFP_724_Nytt_kriterie_utlandssak.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO KODELISTE (id, kodeverk, kode, gyldig_fom)
-VALUES  (seq_kodeliste.nextval, 'ANDRE_KRITERIER', 'UTLANDSSAK', to_date('2000-01-01', 'YYYY-MM-DD'));
-
-INSERT INTO KODELISTE_NAVN_I18N (id, kl_kodeverk, kl_kode, sprak, navn)
-VALUES  (seq_kodeliste.nextval, 'ANDRE_KRITERIER', 'UTLANDSSAK', 'NB', 'Utland');
