7125c49e0d71ff95d5a3bfab010e04279c9d3e1e
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2019-09-09 14:15:49 +0200

    TFP-1081 kun 4205 i denne omgang

--- migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.0_68__TFP_1081_klageenheter.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/1.0.0/V1.0.0_68__TFP_1081_klageenheter.sql
@@ -1,20 +1,8 @@
 UPDATE AVDELING
-SET AVDELING_ENHET   = '4292',
-    NAVN             = 'NAV Klageinstans Midt-Norge',
-    VERSJON          = VERSJON + 1,
-    ENDRET_AV        = 'VL',
-    ENDRET_TID = current_timestamp
+SET AVDELING_ENHET = '4292',
+    NAVN           = 'NAV Klageinstans Midt-Norge',
+    VERSJON        = VERSJON + 1,
+    ENDRET_AV      = 'VL',
+    ENDRET_TID     = current_timestamp
 WHERE AVDELING_ENHET = '4205'
   AND NAVN = 'Klageinstans Midt-Norge';
-
-INSERT INTO AVDELING (ID, AVDELING_ENHET, NAVN, VERSJON, OPPRETTET_AV, OPPRETTET_TID, KREVER_KODE_6)
-VALUES (seq_avdeling.nextval, '4291', 'NAV Klageinstans Oslo/Akershus', 0, 'VL', current_timestamp, 'N');
-
-INSERT INTO AVDELING (ID, AVDELING_ENHET, NAVN, VERSJON, OPPRETTET_AV, OPPRETTET_TID, KREVER_KODE_6)
-VALUES (seq_avdeling.nextval, '4293', 'NAV Klageinstans Øst', 0, 'VL', current_timestamp, 'N');
-
-INSERT INTO AVDELING (ID, AVDELING_ENHET, NAVN, VERSJON, OPPRETTET_AV, OPPRETTET_TID, KREVER_KODE_6)
-VALUES (seq_avdeling.nextval, '4294', 'NAV Klageinstans Vest', 0, 'VL', current_timestamp, 'N');
-
-INSERT INTO AVDELING (ID, AVDELING_ENHET, NAVN, VERSJON, OPPRETTET_AV, OPPRETTET_TID, KREVER_KODE_6)
-VALUES (seq_avdeling.nextval, '4295', 'NAV Klageinstans Nord', 0, 'VL', current_timestamp, 'N');
