cb106f37111433d1193a5b91614a6ea0b693a743
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2019-09-09 15:17:42 +0200

    TFP-1034 fjerner BT-005 inntil videre. Ikke brukt.

--- web/klient/src/client/app/kodeverk/behandlingType.tsx
+++ web/klient/src/client/app/kodeverk/behandlingType.tsx
@@ -3,9 +3,8 @@ const behandlingType = {
   FORSTEGANGSSOKNAD: 'BT-002',
   REVURDERING: 'BT-004',
   KLAGE: 'BT-003',
-  DOKUMENTINNSYN: 'BT-006',
-  SOKNAD: 'BT-005',
   ANKE: 'BT-008',
+  DOKUMENTINNSYN: 'BT-006',
 };
 
 export default behandlingType;
