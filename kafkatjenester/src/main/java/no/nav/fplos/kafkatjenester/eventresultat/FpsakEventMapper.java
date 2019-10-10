e10fde309169573104a3d03ea599aeb7fe6bf8e9
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2019-10-10 14:37:05 +0200

    Svangerskapspenger leverer koden 5096 for papirsøknadregistrering

--- kafkatjenester/src/main/java/no/nav/fplos/kafkatjenester/eventresultat/FpsakEventMapper.java
+++ kafkatjenester/src/main/java/no/nav/fplos/kafkatjenester/eventresultat/FpsakEventMapper.java
@@ -25,7 +25,7 @@ public class FpsakEventMapper {
     public static final String AUTOMATISK_MARKERING_AV_UTENLANDSSAK_AKSJONSPUNKTSKODE = "5068";
     public static final String MANUELT_SATT_PÅ_VENT_AKSJONSPUNKTSKODE = "7001";
     private static final String BESLUTTER_AKSJONSPUNKTSKODE = "5016";
-    private static final List<String> REGISTRER_PAPIRSØKNAD_AKSJONSPUNKTSKODE = asList("5012", "5040", "5057");
+    private static final List<String> REGISTRER_PAPIRSØKNAD_AKSJONSPUNKTSKODE = asList("5012", "5040", "5057", "5096");
     private static final String PÅ_VENT_AKSJONSPUNKT_GRUPPE_STARTER_MED = "7";
 
     private static final List<String> aktiveAksjonspunktkoder = Collections.singletonList("OPPR");
