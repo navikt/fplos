0d1de11b91b09ee3dd1672843f6d4fbe3d6008a0
V134773 <eirik.vimme@nav.no>
2019-11-29 09:56:10 +0100

    TFP-1991 Legger på restklient til batchjobben, og gjør klar for implementasjon av db tilkobling

--- uuid-synkronisering/src/main/java/no/nav/fplos/batch/UUIDSync.java
+++ uuid-synkronisering/src/main/java/no/nav/fplos/batch/UUIDSync.java
@@ -1,5 +1,11 @@
 package no.nav.fplos.batch;
 
+import no.nav.fplos.foreldrepengerbehandling.BehandlingFpsak;
+import no.nav.fplos.foreldrepengerbehandling.ForeldrepengerBehandlingRestKlient;
+import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,12 +13,35 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 public class UUIDSync {
-    @Scheduled(cron = "0 15 06 * * ?")
+
+    private ForeldrepengerBehandlingRestKlient foreldrePengerBehandlingRestKlient;
+
+    private SpringOppgaveRepositoryInterface repo;
+    @Autowired
+    public UUIDSync(SpringOppgaveRepositoryInterface repo) {
+        this.repo = repo;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        OidcRestClient restClient = new OidcRestClient(httpClient);
+        this.foreldrePengerBehandlingRestKlient = new ForeldrepengerBehandlingRestKlient(restClient,  "http://localhost:8080");
+    }
+
+    //@Scheduled(cron = "0 15 06 * * ?")
+    @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
         oppdaterUUID();
     }
 
     private void oppdaterUUID() {
+        //List<Oppgave> oppgaver = (List<Oppgave>) repo.findAll();
 
+        Long behandlingId = 1000101L;
+        finnUUIDFraBehandlingsId(behandlingId);
     }
+
+    private void finnUUIDFraBehandlingsId(Long behandlingId) {
+        BehandlingFpsak sak = foreldrePengerBehandlingRestKlient.getBehandling(behandlingId);
+        System.out.println(sak);
+    }
+
+
 }
