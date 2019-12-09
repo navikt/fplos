fab012d5db389d36d8d5a66100961b513afc2045
V134773 <eirik.vimme@nav.no>
2019-12-09 18:21:57 +0100

    Indre select var ikke glad for å kjøre upper. Kanskje fordi verdiene er null, men det gav et rart resultat

--- domene/loslager/src/main/java/no/nav/foreldrepenger/loslager/repository/OppgaveRepositoryImpl.java
+++ domene/loslager/src/main/java/no/nav/foreldrepenger/loslager/repository/OppgaveRepositoryImpl.java
@@ -105,7 +105,7 @@ public class OppgaveRepositoryImpl implements OppgaveRepository {
                 "NOT EXISTS (select r from Reservasjon r where r.oppgave = o and r.reservertTil > :naa) AND " +
                 "NOT EXISTS (select oetilbesl from OppgaveEgenskap oetilbesl " +
                     "where o.id = oetilbesl.oppgaveId AND oetilbesl.aktiv = true AND oetilbesl.andreKriterierType = :tilbeslutter " +
-                    "AND upper( oetilbesl.sisteSaksbehandlerForTotrinn) = upper( :uid ) ) " +
+                    "AND oetilbesl.sisteSaksbehandlerForTotrinn = upper( :uid ) ) " +
                 "AND a.id = :enhet " +
                 "AND o.aktiv = true " + sortering(queryDto), oppgaveClass)
                 .setParameter("naa", LocalDateTime.now())
