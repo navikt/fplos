89a4bbf285ff19c7ef4b37aacc08b9b5b17bf4a7
V134773 <eirik.vimme@nav.no>
2019-12-17 11:18:13 +0100

    TFP-1991 Fjerner update metoder som ikke er i bruk

--- uuid-synkronisering/src/main/java/no/nav/fplos/uuid/repository/SpringOppgaveEventLoggRepository.java
+++ uuid-synkronisering/src/main/java/no/nav/fplos/uuid/repository/SpringOppgaveEventLoggRepository.java
@@ -1,26 +1,17 @@
 package no.nav.fplos.uuid.repository;
 
 import no.nav.fplos.uuid.dao.OppgaveEventLogg;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface SpringOppgaveEventLoggRepository extends CrudRepository<OppgaveEventLogg, Long> {
 
     @Query("SELECT oe FROM OppgaveEventLogg oe WHERE oe.eksternId is null")
    List<OppgaveEventLogg> finnOppgaveEventerUtenEksternId();
-
-    @Modifying
-    @Query("UPDATE OppgaveEventLogg oe set oe.eksternId = :eksternId where oe.behandlingId = :behandlingId AND oe.eksternId is null")
-    @Transactional
-    void settInnUUIDForOppgaveEventerMedBehandlingId(@Param("behandlingId") Long behandlingId, @Param("eksternId") UUID eksternId);
 
 }
 
--- uuid-synkronisering/src/main/java/no/nav/fplos/uuid/repository/SpringOppgaveRepository.java
+++ uuid-synkronisering/src/main/java/no/nav/fplos/uuid/repository/SpringOppgaveRepository.java
@@ -2,15 +2,11 @@ package no.nav.fplos.uuid.repository;
 
 
 import no.nav.fplos.uuid.dao.Oppgave;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.UUID;
 
 
 @Repository
@@ -19,8 +15,4 @@ public interface SpringOppgaveRepository extends CrudRepository<Oppgave, Long> {
     @Query("SELECT o FROM Oppgave o WHERE o.eksternId is null")
     List<Oppgave> finnOppgaverUtenEksternId();
 
-    @Modifying
-    @Query("UPDATE Oppgave o set o.eksternId = :eksternId where o.behandlingId = :behandlingId and o.eksternId is null")
-    @Transactional
-    void settInnUUIDForOppgaverMedBehandlingId(@Param("behandlingId") Long behandlingId, @Param("eksternId") UUID eksternId);
 }
