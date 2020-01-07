2aacd4c108f3b2f57086fcff048a05c9f51f6ef7
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2020-01-07 15:04:23 +0100

    fix manglende jsonproperty i jsoncreator

--- integrasjon/foreldrepengerbehandling-klient/src/main/java/no/nav/fplos/foreldrepengerbehandling/dto/OverføringÅrsak.java
+++ integrasjon/foreldrepengerbehandling-klient/src/main/java/no/nav/fplos/foreldrepengerbehandling/dto/OverføringÅrsak.java
@@ -1,12 +1,13 @@
 package no.nav.fplos.foreldrepengerbehandling.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum OverføringÅrsak {
     INSTITUSJONSOPPHOLD_ANNEN_FORELDER, SYKDOM_ANNEN_FORELDER, IKKE_RETT_ANNEN_FORELDER, ALENEOMSORG;
 
     @JsonCreator
-    public OverføringÅrsak fraKode(String kode) {
+    public static OverføringÅrsak fraKode(@JsonProperty("kode") String kode) {
         return kode.equals("-") ? null : valueOf(kode);
     }
 
