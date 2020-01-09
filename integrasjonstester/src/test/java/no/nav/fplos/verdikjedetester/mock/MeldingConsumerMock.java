9f6b050d37616d464b9d8fd5cc1cc9adcc550ae5
V134773 <eirik.vimme@nav.no>
2020-01-09 15:23:19 +0100

    TFP-2306 Setter callId i MDC fra kafka header

--- integrasjonstester/src/test/java/no/nav/fplos/verdikjedetester/mock/MeldingConsumerMock.java
+++ integrasjonstester/src/test/java/no/nav/fplos/verdikjedetester/mock/MeldingConsumerMock.java
@@ -1,11 +1,17 @@
 package no.nav.fplos.verdikjedetester.mock;
 
 import no.nav.fplos.kafkatjenester.AksjonspunktMeldingConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Alternative;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Alternative
 @Priority(1)
@@ -13,11 +19,27 @@ import java.util.List;
 public class MeldingConsumerMock implements AksjonspunktMeldingConsumer {
 
     @Override
-    public List<String> hentConsumerMeldingene() {
-        if (!MockEventKafkaMessages.eventer.isEmpty()){
-            return MockEventKafkaMessages.eventer;
+    public ConsumerRecords<String, String> hentConsumerMeldingene() {
+        Map<TopicPartition, List<ConsumerRecord<String, String>>> msgRecordslist = new LinkedHashMap<>();
+        String topic = "topic";
+        TopicPartition topicPartition  = new TopicPartition(topic,0);
+        List<ConsumerRecord<String, String>> topicPartitionEntries = new ArrayList<ConsumerRecord<String, String>>();
+        ConsumerRecords<String, String> response;
+
+        if (!MockEventKafkaMessages.eventer.isEmpty()) {
+            for (String event : MockEventKafkaMessages.eventer){
+                topicPartitionEntries.add(new ConsumerRecord<String, String>("topic", 0, topicPartitionEntries.size(), "", event));
+            }
+            //return MockEventKafkaMessages.eventer;
+        } else {
+            for (String msg : MockKafkaMessages.messages) {
+                topicPartitionEntries.add(new ConsumerRecord<String, String>("topic", 0, topicPartitionEntries.size(), "", msg));
+            }
         }
-        return MockKafkaMessages.messages;
+        msgRecordslist.put(topicPartition, topicPartitionEntries);
+        response = new ConsumerRecords<>(msgRecordslist);
+
+        return response;
     }
 
     @Override
