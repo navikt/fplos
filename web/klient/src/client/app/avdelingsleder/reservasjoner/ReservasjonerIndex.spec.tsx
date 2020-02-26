85bb0a1d12412cbf46eb05afc783b7373db0405c
V134773 <eirik.vimme@nav.no>
2020-02-26 18:01:06 +0100

    TFP-2738 Legger til test for ReservasjonerIndex

---
+++ web/klient/src/client/app/avdelingsleder/reservasjoner/ReservasjonerIndex.spec.tsx
@@ -0,0 +1,25 @@
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import React from 'react';
+
+import { ReservasjonerIndex } from './ReservasjonerIndex';
+import ReservasjonerPanel from './components/ReservasjonerPanel';
+
+describe('<ReservasjonerIndex>', () => {
+  it('skal hente reservasjoner ved lasting av komponent og så vise dem i panel', () => {
+    const fetchAvdelingensReservasjoner = sinon.spy();
+    const wrapper = shallow(<ReservasjonerIndex
+      fetchAvdelingensReservasjoner={fetchAvdelingensReservasjoner}
+      avdelingensReservasjoner={[]}
+      opphevReservasjon={sinon.spy()}
+      valgtAvdelingEnhet="2"
+    />);
+
+    expect(wrapper.find(ReservasjonerPanel)).to.have.length(1);
+    expect(fetchAvdelingensReservasjoner.calledOnce).to.be.true;
+    const { args } = fetchAvdelingensReservasjoner.getCalls()[0];
+    expect(args).to.have.length(1);
+    expect(args[0]).to.eql('2');
+  });
+});
