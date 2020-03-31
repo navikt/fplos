38dd2deef8a1f96ae188be9e398e1aa6676866eb
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2020-03-31 20:26:20 +0200

    Attempting BehandlingIdDto fix

--- web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/saksbehandler/oppgave/dto/BehandlingIdDto.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/saksbehandler/oppgave/dto/BehandlingIdDto.java
@@ -3,12 +3,17 @@ package no.nav.foreldrepenger.los.web.app.tjenester.saksbehandler.oppgave.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import no.nav.foreldrepenger.loslager.BehandlingId;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
 import no.nav.vedtak.sikkerhet.abac.AbacDto;
 
+import javax.validation.constraints.NotNull;
+
 public class BehandlingIdDto implements AbacDto {
 
+    @JsonProperty("behandlingId")
+    @NotNull
     private final BehandlingId value;
 
     @JsonCreator
