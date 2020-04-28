de02392256a868c8e486ca4a4d8964f87716d39b
Anders Palfi <anderspalfi@gmail.com>
2020-04-28 10:38:05 +0200

    Logging naisresttjeneste

--- web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/NaisRestTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/NaisRestTjeneste.java
@@ -6,11 +6,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.swagger.v3.oas.annotations.Operation;
 
 @Path("/health")
 @ApplicationScoped
 public class NaisRestTjeneste {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NaisRestTjeneste.class);
 
     private static final String RESPONSE_CACHE_KEY = "Cache-Control";
     private static final String RESPONSE_CACHE_VAL = "must-revalidate,no-cache,no-store";
@@ -31,6 +36,7 @@ public class NaisRestTjeneste {
     @Path("isAlive")
     @Operation(description = "sjekker om poden lever", tags = "nais", hidden = true)
     public Response isAlive() {
+        LOG.info("isAlive kalles");
         return Response
                 .ok(RESPONSE_OK)
                 .header(RESPONSE_CACHE_KEY, RESPONSE_CACHE_VAL)
@@ -41,6 +47,7 @@ public class NaisRestTjeneste {
     @Path("isReady")
     @Operation(description = "sjekker om poden er klar", tags = "nais", hidden = true)
     public Response isReady() {
+        LOG.info("isReady kalles");
         if (kafkaConsumerStarter.isConsumersRunning()) {
             return Response.ok(RESPONSE_OK)
                     .header(RESPONSE_CACHE_KEY, RESPONSE_CACHE_VAL)
@@ -55,6 +62,7 @@ public class NaisRestTjeneste {
     @Path("preStop")
     @Operation(description = "kalles på før stopp", tags = "nais", hidden = true)
     public Response preStop() {
+        LOG.info("preStop kalles");
         kafkaConsumerStarter.destroy();
         return Response.ok(RESPONSE_OK).build();
     }
