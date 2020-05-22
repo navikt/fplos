6bf868bb9b74332182f23373d6898848b608ac4b
tor-nav <tor.roneid@nav.no>
2020-05-22 14:08:13 +0200

    FIX div

--- web/klient/eslint/eslintrc.common.js
+++ web/klient/eslint/eslintrc.common.js
@@ -32,40 +32,26 @@ const config = {
     'import/extensions': [
       'error',
       'ignorePackages',
-      { 'js': 'never', 'jsx': 'never', 'ts': 'never', 'tsx': 'never' }
+      {
+        ts: 'never', tsx: 'never',
+      },
     ],
     'linebreak-style': OFF,
-    'import/no-named-as-default': OFF,
     'max-len': [ERROR, 160],
+    'no-undef': OFF,
     'react/require-default-props': OFF,
     'react/jsx-filename-extension': OFF,
-    'no-undef': OFF,
     'react/static-property-placement': OFF,
     'react/state-in-constructor': OFF,
-
-    // TODO (TOR) Skrudd av fordi den feilaktig rapporterar typescript-types som ubrukte
-    'no-unused-vars': OFF,
+    'react/prop-types': OFF,
 
     // TODO (TOR) Ignorert inntil videre grunnet kost/nytte
-    'jsx-a11y/anchor-is-valid': OFF,
     'react/jsx-props-no-spreading': OFF,
-
-    '@typescript-eslint/indent': OFF,
-
-    // TODO (TOR) Midlertidig utkommenter
     'jsx-a11y/control-has-associated-label': OFF,
-    '@typescript-eslint/explicit-member-accessibility': OFF,
-    '@typescript-eslint/explicit-function-return-type': OFF,
     '@typescript-eslint/no-explicit-any': OFF,
     '@typescript-eslint/ban-ts-comment': OFF,
     '@typescript-eslint/explicit-module-boundary-types': OFF,
   },
-  overrides: [{
-    files: ['*.tsx'],
-    rules: {
-      'react/prop-types': OFF,
-    },
-  }],
 };
 
 module.exports = config;
