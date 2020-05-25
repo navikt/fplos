b4ca859502ca39a83ba2af3506924b48a085bd4d
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2020-05-25 12:43:41 +0200

    Fjern unødvendig soapfeilmelding ved ikke-treff på fnr.

--- web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/fagsak/app/FagsakApplikasjonTjeneste.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/fagsak/app/FagsakApplikasjonTjeneste.java
@@ -6,13 +6,13 @@ import no.nav.foreldrepenger.loslager.aktør.TpsPersonDto;
 import no.nav.fplos.foreldrepengerbehandling.ForeldrepengerBehandlingRestKlient;
 import no.nav.fplos.foreldrepengerbehandling.dto.fagsak.FagsakDto;
 import no.nav.fplos.person.api.TpsTjeneste;
+import no.nav.vedtak.exception.IntegrasjonException;
 import no.nav.vedtak.exception.ManglerTilgangException;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 
 @ApplicationScoped
@@ -39,23 +39,27 @@ public class FagsakApplikasjonTjeneste {
         if (!søkestreng.matches("\\d+")) {
             return Collections.emptyList();
         }
-
         return predikatErFnr.test(søkestreng)
                 ? hentSakerForFnr(new PersonIdent(søkestreng))
                 : hentFagsakForSaksnummer(new Saksnummer(søkestreng));
     }
 
-    private List<FagsakDto> hentSakerForFnr(PersonIdent fnr) {
-        Optional<TpsPersonDto> funnetNavBruker = tpsTjeneste.hentBrukerForFnr(fnr);
-        if (funnetNavBruker.isEmpty()) {
-            return Collections.emptyList();
-        }
 
-        List<FagsakDto> fagsakDtos = restKlient.getFagsakFraFnr(fnr.getIdent());
-        if (fagsakDtos.isEmpty()) {
-            return Collections.emptyList();
+
+    private List<FagsakDto> hentSakerForFnr(PersonIdent fnr) {
+        try {
+            return tpsTjeneste.hentBrukerForFnr(fnr)
+                    .map(TpsPersonDto::getFnr)
+                    .map(PersonIdent::getIdent)
+                    .map(restKlient::getFagsakFraFnr)
+                    .orElse(Collections.emptyList());
+        } catch (IntegrasjonException e) {
+            if (e.getMessage().contains("Finner ikke bruker med ident")) {
+                // unødvendig feilmelding i frontend hvis vi ikke bytter ut denne med tom liste
+                return Collections.emptyList();
+            }
+            throw e;
         }
-        return fagsakDtos;
     }
 
     private List<FagsakDto> hentFagsakForSaksnummer(Saksnummer saksnummer) {
