5754b6c1bbc2fffd1325386a8a695e57c3c0a767
Steffen Pøhner (NAV) <steffen.h.pohner@nav.no>
2020-09-18 08:45:41 +0200

    Fjern toggle, skru på i prod

--- web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/kodeverk/app/HentKodeverkTjenesteImpl.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/tjenester/kodeverk/app/HentKodeverkTjenesteImpl.java
@@ -8,21 +8,19 @@ import no.nav.foreldrepenger.loslager.oppgave.KøSortering;
 import no.nav.foreldrepenger.loslager.oppgave.Kodeverdi;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 class HentKodeverkTjenesteImpl implements HentKodeverkTjeneste {
 
     private final static Map<String, Collection<? extends Kodeverdi>> KODEVERK = Map.of(
-            BehandlingType.class.getSimpleName(), toggletListe(BehandlingType.values()),
-            FagsakYtelseType.class.getSimpleName(), toggletListe(FagsakYtelseType.values()),
-            KøSortering.class.getSimpleName(), toggletListe(KøSortering.values()),
-            FagsakStatus.class.getSimpleName(), toggletListe(FagsakStatus.values()),
-            AndreKriterierType.class.getSimpleName(), toggletListe(AndreKriterierType.values()));
+            BehandlingType.class.getSimpleName(), List.of(BehandlingType.values()),
+            FagsakYtelseType.class.getSimpleName(), List.of(FagsakYtelseType.values()),
+            KøSortering.class.getSimpleName(), List.of(KøSortering.values()),
+            FagsakStatus.class.getSimpleName(), List.of(FagsakStatus.values()),
+            AndreKriterierType.class.getSimpleName(), List.of(AndreKriterierType.values()));
 
 
     HentKodeverkTjenesteImpl() {
@@ -32,12 +30,5 @@ class HentKodeverkTjenesteImpl implements HentKodeverkTjeneste {
     @Override
     public Map<String, Collection<? extends Kodeverdi>> hentGruppertKodeliste() {
         return KODEVERK;
-    }
-
-    @SafeVarargs
-    private static <T extends Kodeverdi> List<T> toggletListe(T... kodeverdi) {
-        return Arrays.stream(kodeverdi)
-                .filter(v -> !KodeverkFeatureToggle.skalEkskludereFraFrontend(v))
-                .collect(Collectors.toList());
     }
 }
