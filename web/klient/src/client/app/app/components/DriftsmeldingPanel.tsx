34c7d7888680909e8a5b5ffcecd84f585d53fdac
Steffen Pøhner <steffen.h.pohner@nav.no>
2020-10-06 12:31:22 +0200

    Nytt driftsmeldingpanel TFP-3782

---
+++ web/klient/src/client/app/app/components/DriftsmeldingPanel.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent } from 'react';
+import { Row, Column } from 'nav-frontend-grid';
+import { Undertekst } from 'nav-frontend-typografi';
+import advarselImageUrl from 'images/advarsel-sirkel-fyll.svg';
+import Driftsmelding from 'app/driftsmeldingTsType';
+
+import Image from 'sharedComponents/Image';
+import styles from './driftsmeldingPanel.less';
+
+
+interface OwnProps {
+  driftsmeldinger: Driftsmelding[];
+}
+
+
+/**
+ * DriftsmeldingPanel
+ *
+ * Presentasjonskomponent. Definerer hvordan driftsmeldinger vises.
+ */
+const DriftsmeldingPanel: FunctionComponent<OwnProps> = ({
+  driftsmeldinger,
+}) => {
+  if (driftsmeldinger.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      {driftsmeldinger.map((message) => (
+        <Row key={message.id} className={styles.driftsmelding}>
+          <Column xs="11" className={styles.column}>
+            <Image
+              className={styles.driftsInfo}
+              src={advarselImageUrl}
+            />
+            <Undertekst className={styles.wordWrap}>
+              {`${message.melding}`}
+            </Undertekst>
+          </Column>
+        </Row>
+      ))}
+    </div>
+  );
+};
+
+export default DriftsmeldingPanel;
--- web/klient/src/client/app/app/components/ErrorMessagePanel.spec.tsx
+++ web/klient/src/client/app/app/components/ErrorMessagePanel.spec.tsx
@@ -19,7 +19,6 @@ describe('<ErrorMessagePanel>', () => {
         errormessage: 'Error!',
       }}
       removeErrorMessage={() => undefined}
-      driftsmeldinger={[]}
     />);
 
     const div = wrapper.find(Undertekst);
@@ -36,7 +35,6 @@ describe('<ErrorMessagePanel>', () => {
         errormessage: 'Høna &amp; egget og &#34;test1&#34; og &#39;test2&#39;',
       }}
       removeErrorMessage={() => undefined}
-      driftsmeldinger={[]}
     />);
 
     const div = wrapper.find(Undertekst);
@@ -55,39 +53,11 @@ describe('<ErrorMessagePanel>', () => {
         errormessage: 'Dette er en feil',
       }}
       removeErrorMessage={() => undefined}
-      driftsmeldinger={[]}
     />);
 
     const div = wrapper.find(Undertekst);
     expect(div).to.have.length(2);
     expect(div.at(0).childAt(0).text()).to.eql('Dette er en feil ');
     expect(div.at(1).childAt(0).text()).to.eql('Feilet ');
-  });
-
-  it('skal vise Driftsmelding', () => {
-    const melding = {
-      id: '1',
-      melding: 'Dette er driftsmeldingen',
-      aktiv: true,
-      opprettet: '2020-10-10',
-    };
-
-    const wrapper = shallowWithIntl(<ErrorMessagePanel.WrappedComponent
-      intl={intl as IntlShape}
-      errorMessages={[{
-        type: EventType.REQUEST_ERROR,
-        text: 'Feilet',
-      }]}
-      queryStrings={{
-        errormessage: 'Dette er en feil',
-      }}
-      removeErrorMessage={() => undefined}
-      driftsmeldinger={[melding]}
-    />);
-
-    const div = wrapper.find(Undertekst);
-    expect(div).to.have.length(3);
-    expect(div.at(0).childAt(0).text()).to.contain('Dette er driftsmeldingen');
-    expect(div.at(1).childAt(0).text()).to.eq('Dette er en feil ');
   });
 });
