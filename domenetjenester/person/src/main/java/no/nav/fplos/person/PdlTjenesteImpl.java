d9ebff98d383fda895363b4b3bf7aa6dba181e16
Steffen Pøhner <steffen.h.pohner@nav.no>
2020-10-22 13:06:04 +0200

    Pdl rydd

--- domenetjenester/person/src/main/java/no/nav/fplos/person/PdlTjenesteImpl.java
+++ domenetjenester/person/src/main/java/no/nav/fplos/person/PdlTjenesteImpl.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
