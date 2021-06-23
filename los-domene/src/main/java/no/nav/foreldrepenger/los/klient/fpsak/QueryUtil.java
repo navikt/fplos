7f54d58a7111ad9b06c9e592f00cc996b1b0f2e9
Anders Palfi <anderspalfi@gmail.com>
2021-06-23 12:49:36 +0200

    query params riktig på fagsakklient (#1045)

--- los-domene/src/main/java/no/nav/foreldrepenger/los/klient/fpsak/QueryUtil.java
+++ los-domene/src/main/java/no/nav/foreldrepenger/los/klient/fpsak/QueryUtil.java
@@ -1,8 +1,11 @@
 package no.nav.foreldrepenger.los.klient.fpsak;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import javax.ws.rs.client.WebTarget;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.NameValuePair;
@@ -20,8 +23,15 @@ final class QueryUtil {
             throw new IllegalArgumentException("Uventet query " + q + ", forventet likt antall elementer");
         }
         return IntStream.iterate(0, i -> i + 2)
-                .limit(1 + parts.length % 2)
+                .limit(1)
                 .mapToObj(i -> new BasicNameValuePair(parts[i], parts[i + 1]))
                 .collect(Collectors.toList());
+    }
+
+    static WebTarget addQueryParams(URI href, WebTarget target) {
+        for (var q : QueryUtil.split(href.getQuery())) {
+            target = target.queryParam(q.getName(), q.getValue());
+        }
+        return target;
     }
 }
