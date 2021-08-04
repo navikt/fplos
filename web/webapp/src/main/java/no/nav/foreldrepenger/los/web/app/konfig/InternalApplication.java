e6f734d6ae5b7bbad47f419cd35976af00e88096
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2021-08-04 16:07:50 +0200

    Logge feil ved kall til isalive/isready (#1096)

--- web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/konfig/InternalApplication.java
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/konfig/InternalApplication.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.prometheus.client.hotspot.DefaultExports;
 import no.nav.foreldrepenger.los.web.app.metrics.PrometheusRestService;
 import no.nav.foreldrepenger.los.web.app.tjenester.HealthCheckRestService;
@@ -12,11 +15,17 @@ import no.nav.foreldrepenger.los.web.app.tjenester.HealthCheckRestService;
 @ApplicationPath(InternalApplication.API_URL)
 public class InternalApplication extends Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InternalApplication.class);
+
     public static final String API_URL = "/internal";
 
     public InternalApplication() {
         // CDI
-        DefaultExports.initialize();
+        try {
+            DefaultExports.initialize();
+        } catch (Exception e) {
+            LOG.error("LOS startup exception fra defaultexports", e);
+        }
     }
 
     @Override
