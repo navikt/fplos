18c59bd0d56dad7ae09fd6fbbb16857ad9de9215
Tor Røneid <tor.roneid@nav.no>
2021-08-12 10:48:25 +0200

    FIX bug i chrome 92 som gjer at react-intl ikkje fungerar skikkeleg for datoar + oppdatering av dep (#1115)

--- web/klient/.storybook/preview.js
+++ web/klient/.storybook/preview.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import '@formatjs/intl-datetimeformat/polyfill-force';
+import '@formatjs/intl-datetimeformat/locale-data/nb';
+import '@formatjs/intl-numberformat/polyfill-force';
+import '@formatjs/intl-numberformat/locale-data/nb';
 import '../src/client/styles/global.less';
+
 import { switchOnTestMode } from 'data/rest-api';
 
 export const decorators = [(Story) => <div style={{ margin: '40px'}}><Story/></div>];
