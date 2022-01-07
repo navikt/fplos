2ed43e7b5cc1f50c3ceabe665d563df8e6d8efed
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2022-01-07 09:59:40 +0100

    Endre lenke for inntektsmeldinger til en dto som har kun det (#1316)

--- los-domene/src/main/java/no/nav/foreldrepenger/los/klient/fpsak/dto/inntektarbeidytelse/InntektsmeldingerDto.java
+++ los-domene/src/main/java/no/nav/foreldrepenger/los/klient/fpsak/dto/inntektarbeidytelse/InntektsmeldingerDto.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.los.klient.fpsak.dto.inntektarbeidytelse;
 
 import java.util.List;
 
-public record InntektArbeidYtelseDto(List<InntektsmeldingDto> inntektsmeldinger) {
+public record InntektsmeldingerDto(List<InntektsmeldingDto> inntektsmeldinger) {
 
     public List<InntektsmeldingDto> inntektsmeldinger() {
         return inntektsmeldinger == null ? List.of() : inntektsmeldinger;
