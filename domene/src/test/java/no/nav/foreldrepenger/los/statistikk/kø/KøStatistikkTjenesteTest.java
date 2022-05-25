cb0a4afd83bd5da9cb61ad2c592d88ef36a4f7bb
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-05-25 23:27:18 +0200

    Forbedre køstatistikk i første omgang: joine med oppgave for å kun telle behandling én gang. Test mot prod viser ca samme/noe raskere query fra oracle sql developer. Explain plan ca samme cost som gamle. Kan byttes ut med reelt køquery mot oppgavetabellen etter overgang til alltid-ny-oppgave for både fpsak og fptilbake-oppgaver.

--- domene/src/test/java/no/nav/foreldrepenger/los/statistikk/kø/KøStatistikkTjenesteTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/los/statistikk/kø/KøStatistikkTjenesteTest.java
@@ -2,9 +2,7 @@ package no.nav.foreldrepenger.los.statistikk.kø;
 
 import no.nav.foreldrepenger.dbstøtte.DBTestUtil;
 import no.nav.foreldrepenger.extensions.JpaExtension;
-import no.nav.foreldrepenger.los.oppgave.BehandlingType;
 import no.nav.foreldrepenger.los.oppgave.OppgaveRepository;
-import no.nav.foreldrepenger.los.oppgavekø.OppgaveFiltreringKnytning;
 import no.nav.foreldrepenger.los.oppgavekø.OppgaveKøTjeneste;
 import no.nav.foreldrepenger.los.organisasjon.OrganisasjonRepository;
 import no.nav.foreldrepenger.los.oppgave.AndreKriterierType;
@@ -24,7 +22,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
-import java.util.List;
 
 import static no.nav.foreldrepenger.los.organisasjon.Avdeling.AVDELING_DRAMMEN_ENHET;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -75,34 +72,6 @@ class KøStatistikkTjenesteTest {
         assertThat(stats.get(0).behandlingType()).isEqualTo(oppgave.getBehandlingType());
         assertThat(stats.get(0).antallNye()).isEqualTo(0L);
         assertThat(stats.get(0).antallFerdigstilte()).isEqualTo(1L);
-    }
-
-    @Test
-    public void skalLagreStatistikkFørEtterOppdatering() {
-        var knytninger = new OppgaveknytningerFørEtterOppdatering();
-        var førOppdatering = List.of(
-                new OppgaveFiltreringKnytning(1L, 1L, BehandlingType.FØRSTEGANGSSØKNAD),
-                new OppgaveFiltreringKnytning(1L, 2L, BehandlingType.FØRSTEGANGSSØKNAD));
-
-        var etterOppdatering = List.of(
-                new OppgaveFiltreringKnytning(1L, 1L, BehandlingType.FØRSTEGANGSSØKNAD),
-                new OppgaveFiltreringKnytning(1L, 3L, BehandlingType.FØRSTEGANGSSØKNAD));
-        knytninger.setKnytningerFørOppdatering(førOppdatering);
-        knytninger.setKnytningerEtterOppdatering(etterOppdatering);
-        køStatistikk.lagre(knytninger);
-
-        var kø2 = køStatistikk.hentStatistikk(2L);
-        //assertThat(kø2).containsExactly(new NyeOgFerdigstilteOppgaver(LocalDate.now(), BehandlingType.FØRSTEGANGSSØKNAD, 0L, 1L));
-        assertThat(kø2.get(0).behandlingType()).isEqualTo(BehandlingType.FØRSTEGANGSSØKNAD);
-        assertThat(kø2.get(0).antallNye()).isEqualTo(0L);
-        assertThat(kø2.get(0).antallFerdigstilte()).isEqualTo(1L);
-        var kø1 = køStatistikk.hentStatistikk(1L);
-        assertThat(kø1).isEmpty();
-        var kø3 = køStatistikk.hentStatistikk(3L);
-        //assertThat(kø3).containsExactly(new NyeOgFerdigstilteOppgaver(LocalDate.now(), BehandlingType.FØRSTEGANGSSØKNAD, 1L, 0L));
-        assertThat(kø3.get(0).behandlingType()).isEqualTo(BehandlingType.FØRSTEGANGSSØKNAD);
-        assertThat(kø3.get(0).antallNye()).isEqualTo(1L);
-        assertThat(kø3.get(0).antallFerdigstilte()).isEqualTo(0L);
     }
 
     private OppgaveFiltrering kø(Avdeling avdeling) {
