64348c6b8280afd411fd128c80ec58301ae00c73
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-10-09 21:48:27 +0200

    Fiks feil inject

--- domene/src/main/java/no/nav/foreldrepenger/los/organisasjon/ansatt/EnhetstilgangConnection.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/organisasjon/ansatt/EnhetstilgangConnection.java
@@ -5,7 +5,7 @@ import static no.nav.foreldrepenger.los.felles.util.OptionalUtil.tryOrEmpty;
 import java.net.URI;
 import java.util.Optional;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
@@ -15,24 +15,21 @@ import no.nav.vedtak.felles.integrasjon.rest.RestConfig;
 import no.nav.vedtak.felles.integrasjon.rest.RestRequest;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
-@ApplicationScoped
+@Dependent
 @RestClientConfig(tokenConfig = TokenFlow.CONTEXT, endpointProperty = "axsys.url", endpointDefault = "http://axsys.default")
 public class EnhetstilgangConnection {
+
     private static final String PATH = "/api/v1/tilgang/";
-    private RestClient httpClient;
-    private RestConfig restConfig;
-    private boolean enabled;
+
+    private final RestClient httpClient;
+    private final RestConfig restConfig;
+    private final boolean enabled;
 
     @Inject
-    public EnhetstilgangConnection(RestClient httpClient,
-                                   @KonfigVerdi(value = "axsys.enabled", defaultVerdi = "true") boolean enabled) {
-        this.httpClient = httpClient;
+    public EnhetstilgangConnection(@KonfigVerdi(value = "axsys.enabled", defaultVerdi = "true") boolean enabled) {
+        this.httpClient = RestClient.client();
         this.restConfig = RestConfig.forClient(this.getClass());
         this.enabled = enabled;
-    }
-
-    EnhetstilgangConnection() {
-        // CDI
     }
 
     public boolean isEnabled() {
