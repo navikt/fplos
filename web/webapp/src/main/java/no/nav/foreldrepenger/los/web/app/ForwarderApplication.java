8e66e311bf4ca81fed57bfb651a1bdb8a8558da8
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-10-13 13:46:57 +0200

    Forward til fpsak

---
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/ForwarderApplication.java
@@ -0,0 +1,22 @@
+package no.nav.foreldrepenger.los.web.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import java.util.Map;
+import java.util.Set;
+
+
+@ApplicationPath(("/"))
+public class ForwarderApplication extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(FpsakForwarder.class);
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return null;
+    }
+}
---
+++ web/webapp/src/main/java/no/nav/foreldrepenger/los/web/app/FpsakForwarder.java
@@ -0,0 +1,38 @@
+package no.nav.foreldrepenger.los.web.app;
+
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import java.net.URI;
+
+@ApplicationScoped
+@Path("/")
+public class FpsakForwarder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FpsakForwarder.class);
+    private URI fpsakUrl;
+
+
+    public FpsakForwarder() {
+    }
+
+    @Inject
+    public FpsakForwarder(@KonfigVerdi("fpsak.frontend.url") URI fpsakUrl) {
+        this.fpsakUrl = fpsakUrl;
+    }
+
+    @GET
+    public Response forwardFpsak(@Context HttpServletRequest request) {
+        LOG.info("Treff på /fplos, redirecter til {}", fpsakUrl);
+        return Response.seeOther(fpsakUrl).build();
+    }
+}
