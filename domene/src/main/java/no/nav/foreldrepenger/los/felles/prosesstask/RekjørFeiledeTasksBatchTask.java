5386301defb923ad3fdb4147e8f62377a2517dbe
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-10-23 16:53:49 +0200

    Slett gamle tasks

--- domene/src/main/java/no/nav/foreldrepenger/los/felles/prosesstask/RekjørFeiledeTasksBatchTask.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/felles/prosesstask/RekjørFeiledeTasksBatchTask.java
@@ -1,6 +1,6 @@
 package no.nav.foreldrepenger.los.felles.prosesstask;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -9,21 +9,22 @@ import org.slf4j.LoggerFactory;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 
-@ApplicationScoped
+@Dependent
 @ProsessTask(value = "retry.feilendeTasks", cronExpression = "0 20 7 * * *", maxFailedRuns = 1)
 public class RekjørFeiledeTasksBatchTask implements ProsessTaskHandler {
     private static final Logger LOG = LoggerFactory.getLogger(RekjørFeiledeTasksBatchTask.class);
-    private final BatchProsessTaskRepository taskRepository;
+    private final ProsessTaskTjeneste taskTjeneste;
 
     @Inject
-    public RekjørFeiledeTasksBatchTask(BatchProsessTaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
+    public RekjørFeiledeTasksBatchTask(ProsessTaskTjeneste taskTjeneste) {
+        this.taskTjeneste = taskTjeneste;
     }
 
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
-        var rekjørAlleFeiledeTasks = taskRepository.rekjørAlleFeiledeTasks();
+        var rekjørAlleFeiledeTasks = taskTjeneste.restartAlleFeiledeTasks();
         LOG.info("Rekjører alle feilende tasks, oppdaterte {} tasks", rekjørAlleFeiledeTasks);
     }
 }
