da906cf2ac587722f278fc2c730f6f86fc7372f4
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-02-20 21:03:54 +0100

    Felles 4225 task 3119 bruk kontekst

--- domene/src/main/java/no/nav/foreldrepenger/los/felles/util/BrukerIdent.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/felles/util/BrukerIdent.java
@@ -1,15 +1,19 @@
 package no.nav.foreldrepenger.los.felles.util;
 
-import no.nav.foreldrepenger.los.felles.BaseEntitet;
-import no.nav.vedtak.sikkerhet.context.SubjectHandler;
-
 import java.util.Optional;
+
+import no.nav.foreldrepenger.los.felles.BaseEntitet;
+import no.nav.vedtak.sikkerhet.kontekst.Kontekst;
+import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
 
 public class BrukerIdent {
 
+    private BrukerIdent() {
+    }
+
     public static String brukerIdent() {
-        return Optional.ofNullable(SubjectHandler.getSubjectHandler())
-                .map(SubjectHandler::getUid)
+        return Optional.ofNullable(KontekstHolder.getKontekst())
+                .map(Kontekst::getUid)
                 .map(String::toUpperCase)
                 .orElse(BaseEntitet.BRUKERNAVN_NÅR_SIKKERHETSKONTEKST_IKKE_FINNES);
     }
