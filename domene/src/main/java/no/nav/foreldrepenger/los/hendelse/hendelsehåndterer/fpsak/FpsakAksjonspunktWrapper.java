68f00c39178d9e698d140eda4e4c8351d0067134
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-03-13 10:56:31 +0100

    TFP-5427 overgang til fagsakegenskap fase 1

--- domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/fpsak/FpsakAksjonspunktWrapper.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/fpsak/FpsakAksjonspunktWrapper.java
@@ -4,44 +4,55 @@ import static no.nav.foreldrepenger.los.felles.util.StreamUtil.safeStream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 import no.nav.foreldrepenger.los.klient.fpsak.Aksjonspunkt;
 import no.nav.foreldrepenger.los.oppgave.AndreKriterierType;
+import no.nav.vedtak.hendelser.behandling.los.LosFagsakEgenskaperDto;
 
 public class FpsakAksjonspunktWrapper {
 
-    private final List<Aksjonspunkt> aksjonspunkt;
-    private final List<AndreKriterierType> kriterier = new ArrayList<>();
-
-    public FpsakAksjonspunktWrapper(List<Aksjonspunkt> aksjonspunkt) {
-        this.aksjonspunkt = aksjonspunkt;
-        if (finn(Aksjonspunkt::erTilBeslutter)) {
-            kriterier.add(AndreKriterierType.TIL_BESLUTTER);
-        }
-        if (finn(Aksjonspunkt::erRegistrerPapirSøknad)) {
-            kriterier.add(AndreKriterierType.PAPIRSØKNAD);
-        }
-        if (erUtenlandssak()) {
-            kriterier.add(AndreKriterierType.UTLANDSSAK);
-        }
-        if (finn(Aksjonspunkt::erVurderFormkrav)) {
-            kriterier.add(AndreKriterierType.VURDER_FORMKRAV);
-        }
+    private FpsakAksjonspunktWrapper() {
     }
 
-    public List<AndreKriterierType> getKriterier() {
+    public static List<AndreKriterierType> getKriterier(List<Aksjonspunkt> aksjonspunkt, LosFagsakEgenskaperDto sakDto) {
+        List<AndreKriterierType> kriterier = new ArrayList<>();
+        if (finn(aksjonspunkt, Aksjonspunkt::erTilBeslutter)) {
+            kriterier.add(AndreKriterierType.TIL_BESLUTTER);
+        }
+        if (finn(aksjonspunkt, Aksjonspunkt::erRegistrerPapirSøknad)) {
+            kriterier.add(AndreKriterierType.PAPIRSØKNAD);
+        }
+        if (erUtenlandssak(aksjonspunkt, sakDto)) {
+            kriterier.add(AndreKriterierType.UTLANDSSAK);
+        }
+        if (skalVurdereEøs(aksjonspunkt, sakDto)) {
+            kriterier.add(AndreKriterierType.VURDER_EØS_OPPTJENING);
+        }
+        if (finn(aksjonspunkt, Aksjonspunkt::erVurderFormkrav)) {
+            kriterier.add(AndreKriterierType.VURDER_FORMKRAV);
+        }
         return kriterier;
     }
 
-    public boolean erManueltOverstyrtTilNasjonalSak() {
-        return finn(Aksjonspunkt::erManueltOverstyrtTilNasjonalSak);
+    private static boolean skalVurdereEøs(List<Aksjonspunkt> aksjonspunkt, LosFagsakEgenskaperDto dto) {
+        var skalVurdereInnhentingAvSED = finn(aksjonspunkt, Aksjonspunkt::skalVurdereInnhentingAvSED);
+        var egenskapSkalInnhente = Optional.ofNullable(dto.skalInnhenteSED()).isPresent();
+        var ikkeNasjonal = Optional.ofNullable(dto.utlandMarkering()).filter(m -> !LosFagsakEgenskaperDto.UtlandMarkering.NASJONAL.equals(m)).isPresent();
+        return skalVurdereInnhentingAvSED && ikkeNasjonal && egenskapSkalInnhente;
     }
 
-    private boolean erUtenlandssak() {
-        var overstyrtTilNasjonalsak = erManueltOverstyrtTilNasjonalSak();
-        var overstyrtTilUtenlandssak = finn(Aksjonspunkt::erManueltOverstyrtTilUtenlandssak);
-        var skalVurdereInnhentingAvSED = finn(Aksjonspunkt::skalVurdereInnhentingAvSED);
+    private static boolean erUtenlandssak(List<Aksjonspunkt> aksjonspunkt, LosFagsakEgenskaperDto sakDto) {
+        var skalVurdereInnhentingAvSED = finn(aksjonspunkt, Aksjonspunkt::skalVurdereInnhentingAvSED);
+        if (Optional.ofNullable(sakDto).map(LosFagsakEgenskaperDto::utlandMarkering).isPresent()) {
+            return !LosFagsakEgenskaperDto.UtlandMarkering.NASJONAL.equals(sakDto.utlandMarkering()) ||
+                (Objects.equals(Boolean.TRUE, sakDto.skalInnhenteSED()) && skalVurdereInnhentingAvSED);
+        }
+        var overstyrtTilNasjonalsak = finn(aksjonspunkt, Aksjonspunkt::erManueltOverstyrtTilNasjonalSak);
+        var overstyrtTilUtenlandssak = finn(aksjonspunkt, Aksjonspunkt::erManueltOverstyrtTilUtenlandssak);
+
         if (overstyrtTilNasjonalsak) {
             return false;
         } else if (overstyrtTilUtenlandssak) {
@@ -51,7 +62,7 @@ public class FpsakAksjonspunktWrapper {
         }
     }
 
-    private boolean finn(Predicate<Aksjonspunkt> predicate) {
+    private static boolean finn(List<Aksjonspunkt> aksjonspunkt, Predicate<Aksjonspunkt> predicate) {
         return safeStream(aksjonspunkt).anyMatch(predicate);
     }
 
--- domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/tilbakekreving/TilbakekrevingOppgaveEgenskapFinner.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/tilbakekreving/TilbakekrevingOppgaveEgenskapFinner.java
@@ -1,20 +1,30 @@
 package no.nav.foreldrepenger.los.hendelse.hendelsehåndterer.tilbakekreving;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import no.nav.foreldrepenger.los.hendelse.hendelsehåndterer.OppgaveEgenskapFinner;
 import no.nav.foreldrepenger.los.oppgave.AndreKriterierType;
 import no.nav.vedtak.hendelser.behandling.Aksjonspunktstatus;
 import no.nav.vedtak.hendelser.behandling.los.LosBehandlingDto;
+import no.nav.vedtak.hendelser.behandling.los.LosFagsakEgenskaperDto;
 
 public class TilbakekrevingOppgaveEgenskapFinner implements OppgaveEgenskapFinner {
     private final List<AndreKriterierType> andreKriterier;
     private final String saksbehandlerForTotrinn;
 
-    public TilbakekrevingOppgaveEgenskapFinner(List<LosBehandlingDto.LosAksjonspunktDto> aksjonspunkter, String saksbehandler) {
-        this.andreKriterier = aksjonspunkter.stream()
-            .anyMatch(a -> a.definisjon().equals("5005") && Aksjonspunktstatus.OPPRETTET.equals(a.status())) ? List.of(
-            AndreKriterierType.TIL_BESLUTTER) : List.of();
+    public TilbakekrevingOppgaveEgenskapFinner(List<LosBehandlingDto.LosAksjonspunktDto> aksjonspunkter, String saksbehandler,
+                                               LosFagsakEgenskaperDto egenskaperDto) {
+        this.andreKriterier = new ArrayList<>();
+        var utlandsmarkert = Optional.ofNullable(egenskaperDto).map(LosFagsakEgenskaperDto::utlandMarkering)
+            .filter(e -> !LosFagsakEgenskaperDto.UtlandMarkering.NASJONAL.equals(e)).isPresent();
+        if (utlandsmarkert) {
+            this.andreKriterier.add(AndreKriterierType.UTLANDSSAK);
+        }
+        if (aksjonspunkter.stream().anyMatch(a -> a.definisjon().equals("5005") && Aksjonspunktstatus.OPPRETTET.equals(a.status()))) {
+            this.andreKriterier.add(AndreKriterierType.TIL_BESLUTTER);
+        }
         this.saksbehandlerForTotrinn = saksbehandler;
     }
 
