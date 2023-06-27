5b647ac6db60e8fb0de8983e18dc088d4feeecaa
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-06-27 16:11:00 +0200

    Bruk v2 path i alle cluster

--- domene/src/main/java/no/nav/foreldrepenger/los/organisasjon/ansatt/EnhetstilgangConnection.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/organisasjon/ansatt/EnhetstilgangConnection.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.felles.integrasjon.rest.RestClient;
 import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
@@ -21,8 +20,7 @@ import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
     scopesProperty = "axsys.scopes", scopesDefault = "api://prod-fss.org.axsys/.default")
 public class EnhetstilgangConnection {
 
-    // VTP har v1-path
-    private static final String PATH = Environment.current().isLocal() ? "/api/v1/tilgang/" : "/api/v2/tilgang/";
+    private static final String PATH = "/api/v2/tilgang/";
 
     private final RestClient httpClient;
     private final RestConfig restConfig;
