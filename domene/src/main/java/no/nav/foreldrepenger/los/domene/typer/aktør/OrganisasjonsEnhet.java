c0833b2e962fb65d6f0b0ff11e8ac77f48b7a708
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-06-27 16:42:41 +0200

    Ny konensjon respons (#1969)

--- domene/src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/OrganisasjonsEnhet.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/OrganisasjonsEnhet.java
@@ -3,22 +3,10 @@ package no.nav.foreldrepenger.los.domene.typer.aktør;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record OrganisasjonsEnhet(String id, String navn, Set<String> fagområder) {
-
-    @JsonCreator
-    public OrganisasjonsEnhet(@JsonProperty("enhetId") String id,
-                              @JsonProperty("navn") String navn,
-                              @JsonProperty("fagomrader") Set<String> fagområder) {
-        this.id = id;
-        this.navn = navn;
-        this.fagområder = fagområder;
-    }
+public record OrganisasjonsEnhet(String enhetId, String navn, Set<String> temaer) {
 
     public boolean kanBehandleForeldrepenger() {
-        return fagområder.contains("FOR");
+        return temaer.contains("FOR");
     }
 
     @Override
@@ -30,11 +18,11 @@ public record OrganisasjonsEnhet(String id, String navn, Set<String> fagområder
             return false;
         }
         var that = (OrganisasjonsEnhet) o;
-        return id.equals(that.id) && navn.equals(that.navn);
+        return enhetId.equals(that.enhetId) && navn.equals(that.navn);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, navn);
+        return Objects.hash(enhetId, navn);
     }
 }
