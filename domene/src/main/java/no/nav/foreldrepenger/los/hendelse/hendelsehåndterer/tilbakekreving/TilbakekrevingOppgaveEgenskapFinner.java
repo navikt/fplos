25d716ab79380180fc3d1496eacba9a7987f8cee
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-10-10 18:06:07 +0200

    Bedre håndtering retur beslutter (#2008)
    * Bedre håndtering retur beslutter
    
    * Revert fpsakendringer
    
    * Revert fpsakendringer

--- domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/tilbakekreving/TilbakekrevingOppgaveEgenskapFinner.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/tilbakekreving/TilbakekrevingOppgaveEgenskapFinner.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.los.hendelse.hendelsehåndterer.tilbakekreving;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import no.nav.foreldrepenger.los.hendelse.hendelsehåndterer.FagsakEgenskaper;
 import no.nav.foreldrepenger.los.hendelse.hendelsehåndterer.OppgaveEgenskapFinner;
@@ -33,7 +34,8 @@ public class TilbakekrevingOppgaveEgenskapFinner implements OppgaveEgenskapFinne
         if (FagsakEgenskaper.fagsakErMarkertNæring(egenskaperDto)) {
             this.andreKriterier.add(AndreKriterierType.NÆRING);
         }
-        if (aksjonspunkter.stream().anyMatch(a -> a.definisjon().equals("5005") && Aksjonspunktstatus.OPPRETTET.equals(a.status()))) {
+        if (aksjonspunkter.stream().anyMatch(a -> a.definisjon().equals("5005") && Aksjonspunktstatus.OPPRETTET.equals(a.status())) &&
+            aksjonspunkter.stream().noneMatch(a -> !Set.of("5005","7001","7002").contains(a.definisjon()) && Aksjonspunktstatus.OPPRETTET.equals(a.status()))) {
             this.andreKriterier.add(AndreKriterierType.TIL_BESLUTTER);
         }
         this.saksbehandlerForTotrinn = saksbehandler;
