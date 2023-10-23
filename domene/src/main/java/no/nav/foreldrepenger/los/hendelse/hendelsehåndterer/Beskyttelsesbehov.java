74e8bd0560673f9d125c30843f90f255a78d5ba1
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2023-10-23 20:17:17 +0200

    Sende ytelse/behandlingsnummer til PDL (#2020)

--- domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/Beskyttelsesbehov.java
+++ domene/src/main/java/no/nav/foreldrepenger/los/hendelse/hendelsehåndterer/Beskyttelsesbehov.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
-
 import no.nav.foreldrepenger.los.domene.typer.Saksnummer;
 import no.nav.foreldrepenger.los.hendelse.behandlinghendelse.AktørPipKlient;
 import no.nav.foreldrepenger.los.klient.person.PersonTjeneste;
@@ -25,6 +24,6 @@ public class Beskyttelsesbehov {
 
     public Set<AndreKriterierType> getBeskyttelsesKriterier(Oppgave oppgave) {
         var aktører = pipKlient.hentAktørIderSomString(new Saksnummer(String.valueOf(oppgave.getFagsakSaksnummer())));
-        return personTjeneste.harNoenKode7MenIngenHarKode6(aktører) ? Set.of(AndreKriterierType.KODE7_SAK) : Set.of();
+        return personTjeneste.harNoenKode7MenIngenHarKode6(oppgave.getFagsakYtelseType(), aktører) ? Set.of(AndreKriterierType.KODE7_SAK) : Set.of();
     }
 }
