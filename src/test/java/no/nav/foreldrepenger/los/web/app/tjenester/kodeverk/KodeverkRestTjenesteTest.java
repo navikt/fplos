ab3a720e34fcc15573d620dbda494c9ed1cced41
Steffen Pøhner <steffen.h.pohner@nav.no>
2023-12-12 20:49:03 +0100

    Ikke behov for proxyvariant av denne klassen med statiske data

--- src/test/java/no/nav/foreldrepenger/los/web/app/tjenester/kodeverk/KodeverkRestTjenesteTest.java
+++ src/test/java/no/nav/foreldrepenger/los/web/app/tjenester/kodeverk/KodeverkRestTjenesteTest.java
@@ -4,7 +4,6 @@ import no.nav.foreldrepenger.los.oppgave.FagsakStatus;
 import no.nav.foreldrepenger.los.oppgavekø.KøSortering;
 import no.nav.foreldrepenger.los.web.app.jackson.JacksonJsonConfig;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -14,17 +13,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class KodeverkRestTjenesteTest {
 
-    private HentKodeverkTjeneste hentKodeverkTjeneste;
-
-    @BeforeEach
-    void before() {
-        hentKodeverkTjeneste = new HentKodeverkTjeneste();
-    }
-
     @Test
     void skal_hente_kodeverk_og_gruppere_på_kodeverknavn() throws IOException {
 
-        var tjeneste = new KodeverkRestTjeneste(hentKodeverkTjeneste);
+        var tjeneste = new KodeverkRestTjeneste();
         var response = tjeneste.hentGruppertKodeliste();
 
         var rawJson = (String) response.getEntity();
