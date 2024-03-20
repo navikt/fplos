dc515d612a2ecf9b0f4495daab3df38c6adb5643
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2024-03-20 19:58:31 +0100

    Falle 708. Kafka Consumer (#2065)

--- src/main/java/no/nav/foreldrepenger/los/hendelse/behandlinghendelse/BehandlingHendelseHåndterer.java
+++ src/main/java/no/nav/foreldrepenger/los/hendelse/behandlinghendelse/BehandlingHendelseHåndterer.java
@@ -3,15 +3,17 @@ package no.nav.foreldrepenger.los.hendelse.behandlinghendelse;
 import java.time.Instant;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.foreldrepenger.konfig.KonfigVerdi;
 import no.nav.vedtak.exception.VLException;
+import no.nav.vedtak.felles.integrasjon.kafka.KafkaMessageHandler;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.vedtak.hendelser.behandling.BehandlingHendelse;
@@ -23,22 +25,32 @@ import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 @ApplicationScoped
 @ActivateRequestContext
 @Transactional
-public class BehandlingHendelseHåndterer {
+public class BehandlingHendelseHåndterer implements KafkaMessageHandler.KafkaStringMessageHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(BehandlingHendelseHåndterer.class);
-
+    private static final Environment ENV = Environment.current();
+    private static final String PROD_APP_ID = "fplos-behandling"; // Hold konstant pga offset commit !!
     private static final Set<Hendelse> IGNORER = Set.of(Hendelse.BRUKEROPPGAVE, Hendelse.OPPRETTET, Hendelse.MANGLERSØKNAD, Hendelse.MIGRERING);
 
     private ProsessTaskTjeneste taskTjeneste;
     private MottattHendelseRepository hendelseRepository;
 
+    private String topicName;
+
     public BehandlingHendelseHåndterer() {
     }
 
     @Inject
-    public BehandlingHendelseHåndterer(ProsessTaskTjeneste taskTjeneste, MottattHendelseRepository hendelseRepository) {
+    public BehandlingHendelseHåndterer(ProsessTaskTjeneste taskTjeneste, MottattHendelseRepository hendelseRepository,
+                                       @KonfigVerdi(value = "kafka.behandlinghendelse.topic", defaultVerdi = "teamforeldrepenger.behandling-hendelse-v1") String topicName) {
         this.taskTjeneste = taskTjeneste;
         this.hendelseRepository = hendelseRepository;
+        this.topicName = topicName;
+    }
+
+    @Override
+    public void handleRecord(String key, String value) {
+        handleMessage(key, value);
     }
 
     void handleMessage(String key, String payload) {
@@ -54,6 +66,20 @@ public class BehandlingHendelseHåndterer {
             LOG.warn("Vedtatt-Ytelse exception ved håndtering av vedtaksmelding, ignorerer key={}", LoggerUtils.removeLineBreaks(payload), e);
         }
     }
+
+    @Override
+    public String topic() {
+        return topicName;
+    }
+
+    @Override
+    public String groupId() { // Keep stable (or it will read from autoOffsetReset()
+        if (!ENV.isProd()) {
+            return PROD_APP_ID + (ENV.isDev() ? "-dev" : "-vtp");
+        }
+        return PROD_APP_ID;
+    }
+
 
     void handleMessageIntern(BehandlingHendelseV1 behandlingHendelse) {
         var hendelseId = behandlingHendelse.getKildesystem().name() + behandlingHendelse.getHendelseUuid().toString();
