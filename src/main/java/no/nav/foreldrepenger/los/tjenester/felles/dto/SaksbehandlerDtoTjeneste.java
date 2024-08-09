8157d9cdd792cac11058d6468ca3d05ae701904d
Steffen Pøhner <steffen.h.pohner@nav.no>
2024-08-09 10:08:13 +0200

    Fix: send original case brukerident ut (frontend forbedres i neste omgang)

--- src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/SaksbehandlerDtoTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/SaksbehandlerDtoTjeneste.java
@@ -69,7 +69,7 @@ public class SaksbehandlerDtoTjeneste {
     public SaksbehandlerMedAvdelingerDto lagKjentOgUkjentSaksbehandlerMedAvdelingerDto(Saksbehandler saksbehandler) {
         // saksbehandler kan eksistere i basen men være ukjent i azuread
         var ident = saksbehandler.getSaksbehandlerIdent();
-        var saksbehandlerDto = tilSaksbehandlerDto(ident.toLowerCase());
+        var saksbehandlerDto = tilSaksbehandlerDto(ident);
         if (saksbehandlerDto.isPresent()) {
             var avdelinger = ansattTjeneste.hentAvdelingerNavnForAnsatt(ident);
             return new SaksbehandlerMedAvdelingerDto(saksbehandlerDto.get(), avdelinger);
@@ -91,7 +91,7 @@ public class SaksbehandlerDtoTjeneste {
 
     public Optional<BrukerProfil> hentBrukerProfil(String ident) {
         try {
-            return Optional.of(ansattTjeneste.hentBrukerProfil(ident));
+            return Optional.of(ansattTjeneste.hentBrukerProfil(ident.toLowerCase()));
         } catch (IntegrasjonException e) {
             LOG.info("Henting av ansattnavn feilet, fortsetter med empty.", e);
             return Optional.empty();
