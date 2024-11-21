9f28cfc95e933a3a9713ace4ed5bded4d35c2a81
Michal J. Sladek <michal.sladek@nav.no>
2024-11-21 17:57:58 +0100

    test: bruker testcontainer

--- src/test/java/no/nav/foreldrepenger/los/JpaExtension.java
+++ src/test/java/no/nav/foreldrepenger/los/JpaExtension.java
@@ -1,17 +1,21 @@
 package no.nav.foreldrepenger.los;
 
 
-import java.util.TimeZone;
+import org.testcontainers.oracle.OracleContainer;
+import org.testcontainers.utility.DockerImageName;
 
-import no.nav.foreldrepenger.los.Databaseskjemainitialisering;
+import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareExtension;
 
 public class JpaExtension extends EntityManagerAwareExtension {
+    public static final String DEFAULT_TEST_DB_SCHEMA_NAME;
+    private static final String TEST_DB_CONTAINER = Environment.current().getProperty("testcontainer.test.db", String.class, "gvenzl/oracle-free:23-slim-faststart");
+    private static final OracleContainer TEST_DATABASE;
 
-    @Override
-    protected void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Oslo"));
-        Databaseskjemainitialisering.initUnitTestDataSource();
-        Databaseskjemainitialisering.migrerUnittestSkjemaer();
+    static {
+        TEST_DATABASE = new OracleContainer(DockerImageName.parse(TEST_DB_CONTAINER)).withReuse(true);
+        TEST_DATABASE.start();
+        DEFAULT_TEST_DB_SCHEMA_NAME = TEST_DATABASE.getUsername();
+        TestDatabaseInit.settOppDatasourceOgMigrer(TEST_DATABASE.getJdbcUrl(), DEFAULT_TEST_DB_SCHEMA_NAME, TEST_DATABASE.getPassword());
     }
 }
