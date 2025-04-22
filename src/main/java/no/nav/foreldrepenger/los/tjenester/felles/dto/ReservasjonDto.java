f5eac97f3cb72dd041e52da1f411ced02951a6f7
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-04-22 17:36:44 +0200

    Saksnummer og oppgaveSaksNr som String til frontend (#2168)

--- src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/ReservasjonDto.java
+++ src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/ReservasjonDto.java
@@ -6,13 +6,13 @@ import no.nav.foreldrepenger.los.oppgave.BehandlingType;
 import no.nav.foreldrepenger.los.reservasjon.Reservasjon;
 
 public record ReservasjonDto(LocalDateTime reservertTilTidspunkt, String reservertAvUid, String reservertAvNavn, LocalDateTime flyttetTidspunkt,
-                             String flyttetAv, String flyttetAvNavn, String begrunnelse, Long oppgaveId, Long oppgaveSaksNr,
+                             String flyttetAv, String flyttetAvNavn, String begrunnelse, Long oppgaveId, String oppgaveSaksNr,
                              String saksnummer, BehandlingType behandlingType) {
 
     public ReservasjonDto(Reservasjon reservasjon, String reservertAvNavn, String navnFlyttetAv) {
         this(reservasjon.getReservertTil(), reservasjon.getReservertAv(), reservertAvNavn, reservasjon.getFlyttetTidspunkt(),
             reservasjon.getFlyttetAv(), navnFlyttetAv, reservasjon.getBegrunnelse(), reservasjon.getOppgave().getId(),
-            reservasjon.getOppgave().getFagsakSaksnummer(), reservasjon.getOppgave().getSaksnummer(), reservasjon.getOppgave().getBehandlingType());
+            reservasjon.getOppgave().getSaksnummer(), reservasjon.getOppgave().getSaksnummer(), reservasjon.getOppgave().getBehandlingType());
     }
 
     @Override
