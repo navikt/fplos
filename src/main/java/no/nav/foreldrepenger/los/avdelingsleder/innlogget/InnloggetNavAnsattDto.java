5c55c55c4721f4386f7471676675136e05447f0c
Steffen Pøhner <steffen.h.pohner@nav.no>
2025-09-12 12:33:03 +0200

    Legger til en /avdelingsleder/init-fetch for oppgavestyrer-relatert saksbehandlerprofil

---
+++ src/main/java/no/nav/foreldrepenger/los/avdelingsleder/innlogget/InnloggetNavAnsattDto.java
@@ -0,0 +1,65 @@
+package no.nav.foreldrepenger.los.avdelingsleder.innlogget;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public record InnloggetNavAnsattDto(String brukernavn,
+                                    String navn,
+                                    boolean kanOppgavestyre,
+                                    LocalDateTime funksjonellTid) {
+
+    private InnloggetNavAnsattDto(Builder builder) {
+        this(builder.brukernavn, builder.navn, builder.kanOppgavestyre, LocalDateTime.now());
+    }
+
+    public static InnloggetNavAnsattDto ukjentNavAnsatt(String brukernavn, String navn) {
+        return new InnloggetNavAnsattDto(brukernavn, navn,false, LocalDateTime.now());
+    }
+
+
+    @Override
+    public String toString() {
+        return "InnloggetNavAnsattDto{" +
+            "kanOppgavestyre=" + kanOppgavestyre +
+            ", funksjonellTid=" + funksjonellTid + '}';
+    }
+
+    public static class Builder {
+        private final String brukernavn;
+        private final String navn;
+        private boolean kanOppgavestyre;
+
+        public Builder(String brukernavn, String navn) {
+            this.brukernavn = brukernavn;
+            this.navn = navn;
+        }
+
+        public Builder kanOppgavestyre(boolean kanOppgavestyre) {
+            this.kanOppgavestyre = kanOppgavestyre;
+            return this;
+        }
+
+        public InnloggetNavAnsattDto build() {
+            return new InnloggetNavAnsattDto(this);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        InnloggetNavAnsattDto that = (InnloggetNavAnsattDto) o;
+        return kanOppgavestyre == that.kanOppgavestyre
+            && Objects.equals(navn, that.navn)
+            && Objects.equals(brukernavn, that.brukernavn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brukernavn, navn, kanOppgavestyre);
+    }
+}
