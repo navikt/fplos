31a75198d5ff01998fb481357c86e72ca633ce48
Steffen Pøhner <steffen.h.pohner@nav.no>
2025-09-19 09:26:55 +0200

    chore: legger til ryddetask som sletter gamle reservasjoner (#2226)
    chore: legger til ryddetask

---
+++ src/main/java/no/nav/foreldrepenger/los/oppgavekø/SlettUtdaterteTask.java
@@ -0,0 +1,41 @@
+package no.nav.foreldrepenger.los.oppgavekø;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
+import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+@Dependent
+@ProsessTask(value = "vedlikehold.slettutdaterte", cronExpression = "30 15 2 * * *", maxFailedRuns = 1)
+public class SlettUtdaterteTask implements ProsessTaskHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SlettUtdaterteTask.class);
+    private final EntityManager entityManager;
+
+    @Inject
+    public SlettUtdaterteTask(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void doTask(ProsessTaskData prosessTaskData) {
+        var slettetAntall = slettEldreUtløpteReservasjoner();
+        LOG.info("Slettet {} utdaterte reservasjoner", slettetAntall);
+    }
+
+    private int slettEldreUtløpteReservasjoner() {
+        var query = entityManager.createNativeQuery("delete from RESERVASJON where RESERVERT_TIL < :foer");
+        var før = LocalDate.now().minusMonths(13).atStartOfDay();
+        query.setParameter("foer", før);
+        int deletedRows = query.executeUpdate();
+        entityManager.flush();
+        return deletedRows;
+    }
+}
