f11f56cacb861eab975153815ada587b092a8491
Jens-Otto Larsen <46576810+jolarsen@users.noreply.github.com>
2025-10-13 20:25:05 +0200

    bugfix: legg tilbake formattering av Navn som Titlecase (#2241)

--- src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/Person.java
+++ src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/Person.java
@@ -1,3 +1,18 @@
 package no.nav.foreldrepenger.los.domene.typer.aktør;
 
-public record Person(Fødselsnummer fødselsnummer, String navn) { }
+import java.util.Objects;
+
+import no.nav.foreldrepenger.los.felles.util.StringUtils;
+
+public record Person(Fødselsnummer fødselsnummer, String navn) {
+
+    public Person {
+        Objects.requireNonNull(navn);
+        Objects.requireNonNull(fødselsnummer);
+    }
+
+    public static Person personMedFormattertNavn(Fødselsnummer fnr, String navn) {
+        return new Person(fnr, StringUtils.formaterMedStoreOgSmåBokstaver(navn));
+    }
+
+}
