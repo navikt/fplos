00ca27eda1554773d4ca10a19564776152a06170
Steffen Pøhner <steffen.h.pohner@nav.no>
2025-10-16 14:25:17 +0200

    Bump felles til 7.6.8 (#2245)

--- src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/Person.java
+++ src/main/java/no/nav/foreldrepenger/los/domene/typer/aktør/Person.java
@@ -2,17 +2,11 @@ package no.nav.foreldrepenger.los.domene.typer.aktør;
 
 import java.util.Objects;
 
-import no.nav.foreldrepenger.los.felles.util.StringUtils;
-
 public record Person(Fødselsnummer fødselsnummer, String navn) {
 
     public Person {
         Objects.requireNonNull(navn);
         Objects.requireNonNull(fødselsnummer);
-    }
-
-    public static Person personMedFormattertNavn(Fødselsnummer fnr, String navn) {
-        return new Person(fnr, StringUtils.formaterMedStoreOgSmåBokstaver(navn));
     }
 
 }
