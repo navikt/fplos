e791f1265f16edfecbc8f2138dd2252c35cb2d95
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-11-19 16:33:15 +0100

    TFP-6455 sende opp alle valgmuligheter for sortering

--- src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/KøSorteringFeltDto.java
+++ src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/KøSorteringFeltDto.java
@@ -3,12 +3,14 @@ package no.nav.foreldrepenger.los.tjenester.felles.dto;
 import java.util.Arrays;
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.los.oppgavekø.KøSortering;
 
-public record KøSorteringFeltDto(KøSortering køSortering, KøSortering.FeltType feltType, KøSortering.FeltKategori feltKategori) {
+public record KøSorteringFeltDto(@NotNull KøSortering sorteringType, @NotNull String navn,
+                                 @NotNull KøSortering.FeltType feltType, @NotNull KøSortering.FeltKategori feltKategori) {
 
     private static final List<KøSorteringFeltDto> ALLE = Arrays.stream(KøSortering.values())
-        .map(ks -> new KøSorteringFeltDto(ks, ks.getFelttype(), ks.getFeltkategori()))
+        .map(ks -> new KøSorteringFeltDto(ks, ks.getNavn(), ks.getFelttype(), ks.getFeltkategori()))
         .toList();
 
     public static List<KøSorteringFeltDto> alle() {
