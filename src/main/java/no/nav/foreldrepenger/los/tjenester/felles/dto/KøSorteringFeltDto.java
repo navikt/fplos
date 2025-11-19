5eb194d99d72a8685a3e81aab879f7ffc8f70635
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-11-19 17:06:59 +0100

    navngivning

--- src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/KøSorteringFeltDto.java
+++ src/main/java/no/nav/foreldrepenger/los/tjenester/felles/dto/KøSorteringFeltDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.los.oppgavekø.KøSortering;
 
-public record KøSorteringFeltDto(@NotNull KøSortering sorteringType, @NotNull String navn,
+public record KøSorteringFeltDto(@NotNull KøSortering sorteringType, @NotNull String sorteringTittel,
                                  @NotNull KøSortering.FeltType feltType, @NotNull KøSortering.FeltKategori feltKategori) {
 
     private static final List<KøSorteringFeltDto> ALLE = Arrays.stream(KøSortering.values())
