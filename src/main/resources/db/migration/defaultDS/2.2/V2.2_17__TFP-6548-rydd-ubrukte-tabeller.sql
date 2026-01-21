97e0ef744a49b58a338c982dc07fbe85f7c7621e
Steffen Pøhner <steffen.h.pohner@nav.no>
2026-01-21 15:54:15 +0100

    Fjerner view, tabeller og sequencer som ikke lenger er i bruk

---
+++ src/main/resources/db/migration/defaultDS/2.2/V2.2_17__TFP-6548-rydd-ubrukte-tabeller.sql
@@ -0,0 +1,12 @@
+drop view tilbakekreving_oppgave;
+
+drop table tilbakekreving_egenskaper;
+drop table oppgave_event_logg;
+drop table reservasjon_event_logg;
+
+drop sequence SEQ_TILBAKEKREVING_EGENSKAPER;
+drop sequence SEQ_OPPGAVE_EVENT_LOGG;
+drop sequence SEQ_RESERVASJON_EVENT_LOGG;
+drop sequence SEQ_EVENTMOTTAK_FEILLOG;
+drop sequence SEQ_KODELISTE;
+drop sequence SEQ_KODELISTE_NAVN_I18N;
