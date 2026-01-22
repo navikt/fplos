d4e9729fb3c14dd3b82ad18ae69aeade4d883fbf
Steffen Pøhner <steffen.h.pohner@nav.no>
2026-01-22 12:44:25 +0100

    Fix skrivefeil i migreringsscript

--- src/main/resources/db/migration/defaultDS/2.2/V2.2_17__TFP-6548-rydd-ubrukte-tabeller.sql
+++ src/main/resources/db/migration/defaultDS/2.2/V2.2_17__TFP-6548-rydd-ubrukte-tabeller.sql
@@ -7,6 +7,6 @@ drop table reservasjon_event_logg;
 drop sequence SEQ_TILBAKEKREVING_EGENSKAPER;
 drop sequence SEQ_OPPGAVE_EVENT_LOGG;
 drop sequence SEQ_RESERVASJON_EVENT_LOGG;
-drop sequence SEQ_EVENTMOTTAK_FEILLOG;
+drop sequence SEQ_EVENTMOTTAK_FEILLOGG;
 drop sequence SEQ_KODELISTE;
 drop sequence SEQ_KODELISTE_NAVN_I18N;
