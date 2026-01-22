7338276b392bf7c3362c09556693094eaf62d7ee
espenwaaga <espen.waaga@nav.no>
2026-01-22 14:27:27 +0100

    Fix typo og bump migreirng til __18

--- src/main/java/no/nav/foreldrepenger/los/statistikk/AktiveOgTilgjenglige.java
+++ src/main/java/no/nav/foreldrepenger/los/statistikk/AktiveOgTilgjenglige.java
@@ -2,5 +2,5 @@ package no.nav.foreldrepenger.los.statistikk;
 
 import java.time.LocalDateTime;
 
-public record AktiveOgTilgjenglige(LocalDateTime tidspunkt, int aktive, int tilgjenglige) {
+public record AktiveOgTilgjenglige(LocalDateTime tidspunkt, int aktive, int tilgjengelige) {
 }
