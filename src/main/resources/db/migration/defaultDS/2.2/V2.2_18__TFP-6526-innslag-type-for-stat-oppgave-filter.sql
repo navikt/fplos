0b231dd8ea0957346365c4b4cd7827f993655992
espenwaaga <espen.waaga@nav.no>
2026-01-22 15:31:02 +0100

    Oppdatere migrering til å sette not null med en gang

--- src/main/resources/db/migration/defaultDS/2.2/V2.2_18__TFP-6526-innslag-type-for-stat-oppgave-filter.sql
+++ src/main/resources/db/migration/defaultDS/2.2/V2.2_18__TFP-6526-innslag-type-for-stat-oppgave-filter.sql
@@ -1,5 +1,4 @@
-alter table STAT_OPPGAVE_FILTER add INNSLAG_TYPE VARCHAR2(20 char);
+alter table STAT_OPPGAVE_FILTER add INNSLAG_TYPE VARCHAR2(20 char) default 'REGELMESSIG' not null;
 update STAT_OPPGAVE_FILTER set INNSLAG_TYPE = 'REGELMESSIG' where INNSLAG_TYPE is null;
-alter table STAT_OPPGAVE_FILTER modify INNSLAG_TYPE not null;
 
 comment on column STAT_OPPGAVE_FILTER.INNSLAG_TYPE is 'REGELMESSIG for målinger gjort regelmessig hver time, SNAPSHOT for øyeblikksbilder.';
