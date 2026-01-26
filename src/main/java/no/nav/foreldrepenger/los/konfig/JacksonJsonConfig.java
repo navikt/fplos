e045eadbe6682fc353d54ea3eb07032ec6a78631
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2026-01-26 15:21:33 +0100

    TFP-5437 bruk standard JsonMapper overalt

--- src/main/java/no/nav/foreldrepenger/los/konfig/JacksonJsonConfig.java
+++ src/main/java/no/nav/foreldrepenger/los/konfig/JacksonJsonConfig.java
@@ -1,42 +1,21 @@
 package no.nav.foreldrepenger.los.konfig;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import jakarta.ws.rs.ext.ContextResolver;
 import jakarta.ws.rs.ext.Provider;
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
 
 @Provider
 public class JacksonJsonConfig implements ContextResolver<ObjectMapper> {
 
-    private static ObjectMapper OM;
-
-    private final ObjectMapper objectMapper;
-
-    public JacksonJsonConfig() {
-        objectMapper = createbjectMapper();
-    }
-
-    private static synchronized ObjectMapper createbjectMapper() {
-        var mapper = OM;
-        if (mapper == null) {
-            mapper = new ObjectMapper();
-            mapper.registerModule(new Jdk8Module());
-            mapper.registerModule(new JavaTimeModule());
-            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            OM = mapper;
-        }
-        return mapper;
-    }
+    private static final JsonMapper JM = DefaultJsonMapper.getJsonMapper();
 
     @Override
     public ObjectMapper getContext(Class<?> type) {
-        return objectMapper;
+        return JM;
     }
 
 
--- src/test/java/no/nav/foreldrepenger/los/oppgave/OppgaveEnumSerializationDeserializationTest.java
+++ src/test/java/no/nav/foreldrepenger/los/oppgave/OppgaveEnumSerializationDeserializationTest.java
@@ -4,15 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import no.nav.vedtak.mapper.json.DefaultJsonMapper;
 
 class OppgaveEnumSerializationDeserializationTest {
-    private static final ObjectMapper mapper = getObjectMapper();
 
     @Test
     void fagsakStatus() throws Exception {
@@ -29,27 +23,11 @@ class OppgaveEnumSerializationDeserializationTest {
         testRoundtrip(BehandlingType.FØRSTEGANGSSØKNAD);
     }
 
-    private void testRoundtrip(Object initiell) throws JsonProcessingException {
-        var json = toJson(initiell);
-        var roundtripped = fromJson(json, initiell.getClass());
+    private void testRoundtrip(Object initiell)  {
+        var json = DefaultJsonMapper.toJson(initiell);
+        var roundtripped = DefaultJsonMapper.fromJson(json, initiell.getClass());
         assertThat(initiell).isEqualTo(roundtripped);
     }
 
-    static String toJson(Object dto) throws JsonProcessingException {
-        return mapper.writeValueAsString(dto);
-    }
 
-    static <T> T fromJson(String json, Class<T> clazz) throws JsonProcessingException {
-        return mapper.readValue(json, clazz);
-    }
-
-    static ObjectMapper getObjectMapper() {
-        var mapper = new ObjectMapper();
-        mapper.registerModule(new Jdk8Module());
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        return mapper;
-    }
 }
