26fc65db9252326ebabc089101b794160e9af226
Anders Palfi <anders.palfi@nav.no>
2026-01-30 12:24:17 +0100

    TFP-5562 rydder bort gammelt ubrukt felt om oppgavefilter er dynamisk

--- src/main/java/no/nav/foreldrepenger/los/tjenester/avdelingsleder/saksliste/dto/SorteringDto.java
+++ src/main/java/no/nav/foreldrepenger/los/tjenester/avdelingsleder/saksliste/dto/SorteringDto.java
@@ -6,10 +6,9 @@ import no.nav.foreldrepenger.los.oppgave.Periodefilter;
 import no.nav.foreldrepenger.los.oppgavekø.KøSortering;
 import no.nav.foreldrepenger.los.oppgavekø.OppgaveFiltrering;
 
-public record SorteringDto(Long fra, Long til, LocalDate fomDato, LocalDate tomDato, KøSortering sorteringType, boolean erDynamiskPeriode, Periodefilter periodefilter) {
+public record SorteringDto(Long fra, Long til, LocalDate fomDato, LocalDate tomDato, KøSortering sorteringType, Periodefilter periodefilter) {
 
     public SorteringDto(OppgaveFiltrering of) {
-        this(of.getFra(), of.getTil(), of.getFomDato(), of.getTomDato(), of.getSortering(), of.getPeriodefilter() != Periodefilter.FAST_PERIODE,
-            of.getPeriodefilter());
+        this(of.getFra(), of.getTil(), of.getFomDato(), of.getTomDato(), of.getSortering(), of.getPeriodefilter());
     }
 }
