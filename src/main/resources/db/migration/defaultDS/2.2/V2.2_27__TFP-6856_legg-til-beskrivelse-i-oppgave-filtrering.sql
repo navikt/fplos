9764cbd3e8f37514225ea356f29e97f5478f9770
Siri Mykland <siri.mykland@nav.no>
2026-03-04 13:06:00 +0100

    endre begrensning på beskrivelse char lengde

--- src/main/resources/db/migration/defaultDS/2.2/V2.2_27__TFP-6856_legg-til-beskrivelse-i-oppgave-filtrering.sql
+++ src/main/resources/db/migration/defaultDS/2.2/V2.2_27__TFP-6856_legg-til-beskrivelse-i-oppgave-filtrering.sql
@@ -1,3 +1,3 @@
-alter table OPPGAVE_FILTRERING add BESKRIVELSE VARCHAR2(500 char);
+alter table OPPGAVE_FILTRERING add BESKRIVELSE VARCHAR2(1024 char);
 
 comment on column OPPGAVE_FILTRERING.BESKRIVELSE is 'Beskrivelse av oppgavefiltrering.';
